f1aef0074140cc9fcc0c2599c4ef0c09a39499da
David Bello <daesbello@gmail.com>
2026-06-27 11:34:55 -0500

    Fix docker config

--- BlazorCrud_Proyecto01/Program.cs
+++ BlazorCrud_Proyecto01/Program.cs
@@ -1,49 +1,48 @@
 using BlazorCrud.Repositorio;
 using BlazorCrud_Proyecto01.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-//Configurar conexión a MSSQLLocalDB
-//builder.Services.AddDbContext<ApplicationDbContext>(
-//    options => options.UseSqlServer(
-//    builder.Configuration.GetConnectionString("ConexionSQLLocalDB")));
-
-//Nuevo: ------------------
-//builder.Services.AddDbContext<ApplicationDbContext>(
-//    options => options.UseSqlServer(
-//builder.Configuration.GetConnectionString("DefaultConnection")));
-//
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IRepositorio, Repositorio>();
+
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate();
+
+    var retries = 10;
+    while (retries > 0)
+    {
+        try
+        {
+            dbContext.Database.Migrate();
+            break;
+        }
+        catch
+        {
+            retries--;
+            Thread.Sleep(5000);
+        }
+    }
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
-//Nuevo: ------------------
-app.UseAuthentication();
-//
 app.UseStaticFiles();
 
 app.UseRouting();
